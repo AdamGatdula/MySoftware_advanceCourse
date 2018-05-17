031c1fd9fd0b83d3e3c2b87d65f0ec95aba47bcc
AdamGatdula <adam.gatdula1@gmail.com>
2018-05-17 21:49:32 +0300

    Initial commit

---
+++ src/MySoftware.java
@@ -0,0 +1,12 @@
+public class MySoftware {
+    public static void main(String[] args) {
+        String NewButton = new String("Click");
+        String NewScreen = new String("New Screen");
+        
+        System.out.println(NewButton);
+        System.out.println(NewScreen);
+    }
+
+
+
+}
